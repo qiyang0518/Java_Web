30f52b12bc07b64f400f98337b753192fcb1cb7f
qiyang <qiyang@126.com>
2023-05-13 09:22:32 +0800

    Java_Web commit1

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * @author 齐阳
+ * @version 1.0
+ */
+@SuppressWarnings({"all"})
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world!");
+    }
+}
